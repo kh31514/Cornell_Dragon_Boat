ce4ff2a02fca690689a304c937ce5d0aa27c9c04
Katie Huntley <kah294@cornell.edu>
2024-08-29 21:32:02 -0400

    re-did file structure, started header component

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import HeaderComponent from './components/HeaderComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <HeaderComponent />
+      </header>
+      <body>
+      </body>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/HeaderComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './HeaderComponent.css';
+
+const HeaderComponent = () => {
+  return (
+    <h1>Cornell Dragon Boat Club</h1>
+  );
+}
+
+export default HeaderComponent;
