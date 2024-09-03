5454c6a5bcb9cab6df380472187dee47a3a2f30e
Katie Huntley <kah294@cornell.edu>
2024-09-03 14:16:56 -0400

    added FAQ section with CSS

--- my-app/src/pages/HomePage.js
+++ my-app/src/pages/HomePage.js
@@ -2,6 +2,7 @@ import Header from '../components/Header';
 import ImgBanner from '../components/ImgBanner';
 import Schedule from "../components/Schedule";
 import TextSection from '../components/TextSection';
+import FAQ from '../components/FAQ';
 
 import dragonBoatsImg from "../assets/dragonBoats.jpg";
 import logo from "../assets/logo.png";
@@ -9,6 +10,8 @@ import dragonHeadsImg from "../assets/dragonHeads.png";
 
 let whoWeAreText = "Dragon Boat at Cornell is a competitive dragon boat team based in Ithaca, NY. Founded a couple years ago, we aim to provide Cornell University students a culturally-enriching exercise routine and foster community with local residents. We collaborate with the Ithaca Dragon Boat Club to offer practices 3 times per week launching from Cass Park. See practice schedule above for exact dates/times."
 let dragonBoatingText = "Dragon boating is a traditional Chinese sport where a team of (roughly 20) paddlers work together to propel a boat similar to a long canoe.";
+let president = "Katie Huntley";
+let presEmail = "kah294@cornell.edu";
 
 function App() {
   return (
@@ -21,7 +24,7 @@ function App() {
           source={dragonBoatsImg}
         />
       </header>
-      <body>
+      <main>
         <Schedule />
         <TextSection
           title="Who We Are"
@@ -33,13 +36,17 @@ function App() {
         />
         <TextSection
           title="What is Dragon Boating?"
-          body={whoWeAreText}
+          body={dragonBoatingText}
           bkgrnd="white"
           imgPos="left"
           imgSrc={dragonHeadsImg}
           imgSize="square"
         />
-      </body>
+        <FAQ
+          president={president}
+          presEmail={presEmail}
+        />
+      </main>
     </div>
   );
 }
